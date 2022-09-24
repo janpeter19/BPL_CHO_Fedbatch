7af4c442a5365ad8f885d65eb687cbf1f40fef0a
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2022-09-24 13:32:19 +0200

    Small change in system_info()

--- BPL_CHO_Fedbatch_explore.py
+++ BPL_CHO_Fedbatch_explore.py
@@ -489,7 +489,7 @@ def describe(name, decimals=3):
 
 #------------------------------------------------------------------------------------------------------------------
 #  General code 
-FMU_explore = 'FMU-explore ver 0.9.3'
+FMU_explore = 'FMU-explore ver 0.9.4'
 #------------------------------------------------------------------------------------------------------------------
 
 # Define function par() for parameter update
@@ -746,6 +746,11 @@ def system_info():
    print('System information')
    print(' -OS:', platform.system())
    print(' -Python:', platform.python_version())
+   try:
+       scipy_ver = scipy.__version__
+       print(' -Scipy:',scipy_ver)
+   except NameError:
+       print(' -Scipy: not installed in the notebook')
    print(' -PyFMI:', version('pyfmi'))
    print(' -FMU by:', model.get_generation_tool())
    print(' -FMI:', model.get_version())
